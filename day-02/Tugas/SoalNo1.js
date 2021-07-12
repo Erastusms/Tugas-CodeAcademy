f91b04bbb9b5be1dcb447c727c25348844fdb7c6
Erastus Mauliate <emauliate17@gmail.com>
2021-07-12 13:33:51 +0700

    Update nomor 1

--- day-02/Tugas/SoalNo1.js
+++ day-02/Tugas/SoalNo1.js
@@ -5,25 +5,33 @@ let sayur = ["Kangkung", "Bayam", "Tomat", "Bawang", "Bayam"];
 let gabung = fruits.concat(sayur);
 let gabungReverse = gabung.reverse();
 
+// a
 fruits.push("Anggur", "Banana", "Sirsak");
 console.log(fruits);
 
+// b
 fruits.pop();
 console.log(fruits);
 
+// c
 fruits.splice(2, 1);
 console.log(fruits);
 
+// d
 fruits.splice(2, 3);
 console.log(fruits);
 
+// e
 fruits.splice(2, 0, "Rambutan", "Bangkuang");
 console.log(fruits);
 
+// f
 fruits.splice(4, 1, "Nangka");
 console.log(fruits);
 
+// g
 console.log(gabung);
+// h
 console.log(gabungReverse);
 
 function tukarSayurBuah(vege, sayur, buah){
@@ -37,4 +45,5 @@ function tukarSayurBuah(vege, sayur, buah){
     return vege;
 }
 
+// i
 console.log(tukarSayurBuah(gabung,'TOMAT','JERUK'))

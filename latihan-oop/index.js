77eb863d44eb63b490c2a0ae6ac7d0e741e8561e
Erastus Mauliate <emauliate17@gmail.com>
2021-07-13 15:02:04 +0700

    Update latihan-oop

--- latihan-oop/index.js
+++ latihan-oop/index.js
@@ -2,10 +2,10 @@ const Kitchen = require("./Kitchen")
 
 let kitchen = new Kitchen()
 
-// kitchen.bake("Martabak Strawberry", 30000, "Strawberry", false)
-// kitchen.bake("Cheese Cake", 40000, "Sweet", false)
-// kitchen.addSugar("Choco Lava")
-// kitchen.addSugar("Martabak Strawberry")
+kitchen.bake("Martabak Strawberry", 30000, "Strawberry", false)
+kitchen.bake("Cheese Cake", 40000, "Sweet", false)
+kitchen.addSugar("Choco Lava")
+kitchen.addSugar("Martabak Strawberry")
 
 kitchen.eat("Choco Lava");
-// kitchen.showCookies()
+kitchen.showCookies()

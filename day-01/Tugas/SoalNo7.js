e895c1954ae38c806237e4f093414a050950b3c2
Erastus Mauliate <emauliate17@gmail.com>
2021-07-13 13:26:56 +0700

    Update nomor 7

--- day-01/Tugas/SoalNo7.js
+++ day-01/Tugas/SoalNo7.js
@@ -18,4 +18,17 @@ function deretSatu(k) {
   }
 }
 
-deretSatu(5);
+// function deretSatu(k) {
+//   var temp = "";
+
+//   for (let i = 0; i < k; i++) {
+//     for (let j = 0; j < i + 1; j++) {
+//       temp += 1;
+//     }
+//     if (i < k - 1) temp += ",";
+//   }
+//   return temp;
+// }
+
+// deretSatu(5);
+console.log(deretSatu(5));
